f33a1a1497bd1f8c1b86fe8466ac61c7c61d08b6
Padamaraj Kore <padamarajkore@Padamarajs-MacBook-Air.local>
2025-07-26 13:52:03 +0530

    acsxg

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"></link>
     <App />
   </React.StrictMode>
 );
